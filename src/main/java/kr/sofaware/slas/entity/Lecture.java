f6431bddd8f6bfa099e2abf124e1028f6446f1c6
sohyun <61389446+sohyun-99@users.noreply.github.com>
2022-02-07 16:00:48 +0900

    2022.02.07 entity (PK 수정)

--- src/main/java/kr/sofaware/slas/entity/Lecture.java
+++ src/main/java/kr/sofaware/slas/entity/Lecture.java
@@ -1,15 +1,15 @@
 package kr.sofaware.slas.entity;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import org.hibernate.Hibernate;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@EqualsAndHashCode
+@IdClass(LecturePK.class)
 @Entity
 @Getter
 public class Lecture implements Serializable {
@@ -25,18 +25,23 @@ public class Lecture implements Serializable {
 
     private double grade;                  // 학점
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(student, syllabus);
-    }
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(student, syllabus);
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj) return true;
+//        if (obj == null || Hibernate.getClass(this) != Hibernate.getClass(obj)) return false;
+//
+//        Lecture o = (Lecture) obj;
+//        return Objects.equals(student, o.getStudent()) &&
+//                Objects.equals(syllabus, o.getSyllabus());
+//    }
+}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || Hibernate.getClass(this) != Hibernate.getClass(obj)) return false;
-
-        Lecture o = (Lecture) obj;
-        return Objects.equals(student, o.getStudent()) &&
-                Objects.equals(syllabus, o.getSyllabus());
-    }
+class LecturePK implements Serializable{
+    private Student student;            // 학생
+    private Syllabus syllabus;          // 강의
 }
--- src/main/java/kr/sofaware/slas/entity/Quiz.java
+++ src/main/java/kr/sofaware/slas/entity/Quiz.java
@@ -1,18 +1,18 @@
 package kr.sofaware.slas.entity;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import org.hibernate.Hibernate;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@EqualsAndHashCode
+@IdClass(QuizPK.class)
 @Entity
 @Getter
-public class Quiz implements Serializable {
+public class Quiz {//implements Serializable {
     @Id
     @ManyToOne
     @JoinColumn(name = "syllabus_id")
@@ -28,20 +28,27 @@ public class Quiz implements Serializable {
     private String attachmentName;      // 첨부 파일 이름
     private String attachmentPath;      // 첨부 파일 경로
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(syllabus, id, questionNum);
-    }
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(syllabus, id, questionNum);
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj) return true;
+//        if (obj == null || Hibernate.getClass(this) != Hibernate.getClass(obj))
+//            return false;
+//
+//        Quiz o = (Quiz) obj;
+//        return Objects.equals(syllabus, o.getSyllabus()) &&
+//                Objects.equals(id, o.getId()) &&
+//                Objects.equals(questionNum, o.getQuestionNum());
+//    }
+}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || Hibernate.getClass(this) != Hibernate.getClass(obj))
-            return false;
+class QuizPK implements Serializable{
+    private Syllabus syllabus;          // 강의
+    private String id;              // 퀴즈 아이디 (<주차>-<회차>, 1-1, 1-2, ...)
+    private int questionNum;        // 문제 번호
 
-        Quiz o = (Quiz) obj;
-        return Objects.equals(syllabus, o.getSyllabus()) &&
-                Objects.equals(id, o.getId()) &&
-                Objects.equals(questionNum, o.getQuestionNum());
-    }
 }
