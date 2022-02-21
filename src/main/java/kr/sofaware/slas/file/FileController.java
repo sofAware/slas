27c8a856b42009966f52de5489249c3f7819b1ed
GyeongHo Yang <chchch1213@naver.com>
2022-02-21 19:23:37 +0900

    파일 다운로드 구현 완료 인코딩 문제까지 해결

--- src/main/java/kr/sofaware/slas/file/FileController.java
+++ src/main/java/kr/sofaware/slas/file/FileController.java
@@ -2,16 +2,20 @@ package kr.sofaware.slas.file;
 
 import kr.sofaware.slas.service.FileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.text.html.Option;
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -33,15 +37,25 @@ public class FileController {
     }
 
     @GetMapping("/**")
-    @ResponseBody
-    public byte[] download(HttpServletRequest request, HttpServletResponse response,
+    public void download(HttpServletRequest request, HttpServletResponse response,
                            @RequestParam("filename") Optional<String> filename) throws IOException {
 
+        // 파일 위치 세팅 후 유무 체크
         Path path = Paths.get(System.getProperty("user.dir"), request.getRequestURI());
-        response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
-        response.setHeader("Content-Disposition",
-                filename.orElse(path.toString().substring(path.toString().lastIndexOf("/") + 1)));
+        if (!Files.exists(path)) return;
 
-        return Files.exists(path) ? Files.readAllBytes(path) : null;
+        // 파일 이름 인코딩
+        String encodedFilename = URLEncoder.encode(
+                filename.orElse("download" + path.toString().substring(path.toString().lastIndexOf("."))),
+                StandardCharsets.UTF_8);
+
+        // 헤더 헤팅
+        response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+        response.setContentLengthLong(path.toFile().length());
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=" +encodedFilename);
+        ServletOutputStream sos = response.getOutputStream();
+
+        // 전송
+        sos.write(Files.readAllBytes(path));
     }
 }
