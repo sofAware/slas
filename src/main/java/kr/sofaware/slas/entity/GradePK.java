9807965dab09e8d7daeebe92707b46ebba386d45
npole0103 <npole0103@naver.com>
2022-03-24 19:02:08 +0900

    교수 성적 입력시 학생 평점/전공평점/석차 동기화

--- src/main/java/kr/sofaware/slas/entity/GradePK.java
+++ src/main/java/kr/sofaware/slas/entity/GradePK.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 
 @EqualsAndHashCode
 public class GradePK implements Serializable {
-    private Member student;            // 학생
+    private String student;            // 학생
     private int year;                   // 년도
     private int semester;
 }
--- src/main/java/kr/sofaware/slas/repository/GradeRepository.java
+++ src/main/java/kr/sofaware/slas/repository/GradeRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface GradeRepository extends JpaRepository<Grade, GradePK> {
     List<Grade> findAllByStudent_Id(String studentId);
+    Optional<Grade> findByStudent_IdAndYearAndSemester(String studentId, int year, int semester);
+    List<Optional<Grade>> findAllByYearAndSemesterOrderByGradeAvgDesc(int year, int semester);
 }
