5561e63b55227071296344ebf97a7dd1525265fb
GyeongHo Yang <chchch1213@naver.com>
2022-04-12 20:17:41 +0900

    Gmail SMTP 서버 사용하여 메일 테스트

---
+++ src/test/java/kr/sofaware/slas/mail/MailSendTest.java
@@ -0,0 +1,30 @@
+package kr.sofaware.slas.mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+
+@SpringBootTest
+public class MailSendTest {
+
+    @Autowired
+    JavaMailSender mailSender;
+
+    @Test
+    public void send() throws MessagingException, IOException {
+        MailHandler mailHandler = new MailHandler(mailSender);
+
+        mailHandler.setTo("chchch1213@naver.com");
+        mailHandler.setSubject("스프링의 제목제목제목제목제목제목제목제목");
+
+        String htmlContent = "<h1>쉬운길을 택했다</h1> <img src='cid:slas-logo'>";
+        mailHandler.setText(htmlContent, true);
+        mailHandler.setAttach("en.json", "static/assets/lang/en.json");
+        mailHandler.setInline("slas-logo", "static/assets/images/favicon.png");
+        mailHandler.send();
+    }
+}
